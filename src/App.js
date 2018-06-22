44eb74bdca129a92fd5fdb77edc8d87debe27da7
Sebastian Springer <info@sebastian-springer.com>
2018-06-22 10:24:39 +0200

    match component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Team } from './Team';
+import { Match } from './Match';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Team />
+        <Match />
       </div>
     );
   }
---
+++ src/Match.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Match = () => {
+  return (
+    <div>
+      <div>Deutschland</div>
+      <div>Mexiko</div>
+      <div>0</div>
+      <div>1</div>
+    </div>
+  );
+};
