356749203d03bd39b73461f7098f8ae1504267dd
Sebastian Springer <info@sebastian-springer.com>
2018-06-22 10:15:57 +0200

    cleanup

---
+++ src/Team.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Team = () => {
+  return (
+    <div>
+      <div>Deutschland</div>
+      <div>Gruppe: F</div>
+    </div>
+  );
+};
+
+// don't export default () => {};
